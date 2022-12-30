b1028d20e7a25b69a0ed2fb6f8cea0d2e05b6656
maria.markowiak@gmail.com <maria.markowiak@gmail.com>
2022-12-30 20:33:04 +0100

    create project structure

---
+++ angular-front/src/app/shared/layout/message-popup/message-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-popup',
+  templateUrl: './message-popup.component.html',
+  styleUrls: ['./message-popup.component.scss']
+})
+export class MessagePopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
