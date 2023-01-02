2c0b05d7d4e56c0fa35b14c3bd64e75d5b251191
maria.markowiak@gmail.com <maria.markowiak@gmail.com>
2023-01-02 18:35:15 +0100

    Add nav structure

--- angular-front/src/app/history/history-routing.module.ts
+++ angular-front/src/app/history/history-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { OrderPageComponent } from './order-page';
+import { OrdersPageComponent } from './orders-page';
+import { ReclamationPageComponent } from './reclamation-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'order', component: OrderPageComponent },
+  { path: 'orders', component: OrdersPageComponent },
+  { path: 'reclamation', component: ReclamationPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
