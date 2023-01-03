109ec62700d005134b90ae07d2d8ed928fc493c4
maria.markowiak@gmail.com <maria.markowiak@gmail.com>
2023-01-03 14:30:33 +0100

    init loyalty-program-page

--- angular-front/src/app/loyalty-program/loyalty-program-routing.module.ts
+++ angular-front/src/app/loyalty-program/loyalty-program-routing.module.ts
@@ -1,7 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
+import { LoyaltyProgramPageComponent } from './loyalty-program-page';
 import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'loyalty-program', component: LoyaltyProgramPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
