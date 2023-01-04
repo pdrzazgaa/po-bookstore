57848170b4cffbc899bd567693bbee6abb70acee
maria.markowiak@gmail.com <maria.markowiak@gmail.com>
2023-01-04 18:49:27 +0100

    Add popup message

--- angular-front/src/app/catalog/product-page/product-page.component.ts
+++ angular-front/src/app/catalog/product-page/product-page.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable no-unused-vars */
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductDetails, ProductService } from 'src/app/core';
+import { Subscription, timer } from 'rxjs';
+
+enum PopupMessage {
+  accept = 'Dodano produkt do koszyka',
+  deny = 'Brak produktu na stanie magazynu',
+}
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.scss'],
 })
-export class ProductPageComponent implements OnInit {
+export class ProductPageComponent implements OnInit, OnDestroy {
   private productService: ProductService;
   product!: ProductDetails;
+  showPopup: boolean = false;
+  popupMessage: string = PopupMessage.accept;
+  popupMode: 'accept' | 'deny' = 'accept';
+  disabledButton: boolean = false;
+  private showPopupSub?: Subscription;
 
   constructor(productService: ProductService) {
     this.productService = productService;
@@ -16,5 +28,20 @@ export class ProductPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.product = this.productService.getProduct();
+  }
+
+  onButtonClick() {
+    this.showPopup = true;
+    this.disabledButton = true;
+    this.popupMessage = PopupMessage[this.popupMode];
+    this.showPopupSub?.unsubscribe();
+    this.showPopupSub = timer(3000).subscribe(() => {
+      this.showPopup = false;
+      this.disabledButton = false;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.showPopupSub?.unsubscribe();
   }
 }
--- angular-front/src/app/shared/layout/message-popup/message-popup.component.ts
+++ angular-front/src/app/shared/layout/message-popup/message-popup.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-message-popup',
   templateUrl: './message-popup.component.html',
   styleUrls: ['./message-popup.component.scss'],
 })
-export class MessagePopupComponent implements OnInit {
+export class MessagePopupComponent implements OnInit, OnDestroy {
+  @Input() mode: 'accept' | 'deny' = 'accept';
+  @Input() message: string = '';
+  appear: boolean = false;
+  disappear: boolean = false;
+  private appearSub?: Subscription;
+  private disappearSub?: Subscription;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.appearSub = timer(500).subscribe(() => (this.appear = true));
+    this.disappearSub = timer(2500).subscribe(() => (this.disappear = true));
+  }
+
+  ngOnDestroy(): void {
+    this.appearSub?.unsubscribe();
+    this.disappearSub?.unsubscribe();
+  }
 }
