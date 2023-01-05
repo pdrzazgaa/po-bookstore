5ac7c3b8a3084b897ffde801da1a99947e29bc4c
maria.markowiak@gmail.com <maria.markowiak@gmail.com>
2023-01-05 12:59:37 +0100

    fix side menu

--- angular-front/src/app/catalog/products-page/side-menu/side-menu.component.ts
+++ angular-front/src/app/catalog/products-page/side-menu/side-menu.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss'],
 })
-export class SideMenuComponent implements OnInit {
-  public category: string = 'Poradniki i albumy';
-  public subcategory: string = 'Podroze';
-
+export class SideMenuComponent implements OnInit, OnDestroy {
+  public category: string = '';
+  public subcategory: string = '';
   public priceRanges: number[] = [20, 50, 100, 200];
-  constructor() {}
+  private route: ActivatedRoute;
+  private routeSub?: Subscription;
 
-  ngOnInit(): void {}
+  constructor(route: ActivatedRoute) {
+    this.route = route;
+  }
+
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe((params) => {
+      console.log(params);
+      this.category = params['category'];
+      this.subcategory = params['subcategory'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub?.unsubscribe();
+  }
 }
