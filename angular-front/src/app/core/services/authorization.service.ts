c1a61a08f3b7ae74e56527b5da9f831bf6f8121d
maria.markowiak@gmail.com <maria.markowiak@gmail.com>
2023-01-16 14:52:40 +0100

    add veryfing logging in with api

--- angular-front/src/app/core/services/authorization.service.ts
+++ angular-front/src/app/core/services/authorization.service.ts
@@ -1,14 +1,29 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
 
 @Injectable()
 export class AuthorizationService {
+  private http: HttpClient;
+  private baseUrl: string = 'http://localhost:6060/verify';
   isLoggedInChanged = new EventEmitter<{ loggedIn: boolean }>();
 
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
   login(username: string, password: string) {
-    console.log(username, password);
-    localStorage.setItem('userId', '1');
-    this.isLoggedInChanged.emit({ loggedIn: true });
+    const headers = { 'content-type': 'application/json' };
+    this.http
+      .post(this.baseUrl, JSON.stringify({ email: username, password: password }), {
+        headers: headers,
+      })
+      .subscribe((res: any) => {
+        if (res.id > 0) {
+          this.isLoggedInChanged.emit({ loggedIn: true });
+          localStorage.setItem('userId', `${res.id}`);
+        }
+      });
     return of(this.isLoggedIn());
   }
 
