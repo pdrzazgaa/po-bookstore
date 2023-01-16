fc5623ea9b43b3521c0ffa8ca9bc06401ee5065c
Paulina Drzazga <paulina@halpress.team>
2023-01-16 15:27:35 +0100

    Added getting all product and by category

--- bookstore_backend/bookstore/src/main/java/com/listek/bookstore/controllers/BooksController.java
+++ bookstore_backend/bookstore/src/main/java/com/listek/bookstore/controllers/BooksController.java
@@ -1,8 +1,48 @@
 package com.listek.bookstore.controllers;
 
+import com.listek.bookstore.fromToModels.ProductListToData;
+import com.listek.bookstore.models.Order;
+import com.listek.bookstore.repositories.BookRepository;
+import com.listek.bookstore.repositories.ProductRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BooksController {
+
+    BookRepository bookRepository;
+
+    public BooksController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/products")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<ProductListToData> getProducts(){
+        System.out.println("Get products");
+        List<Object[]> books = bookRepository.findBooks();
+        List<ProductListToData> booksToData = new ArrayList<>();
+        for (Object[] bookObj:books){
+            booksToData.add(new ProductListToData(bookObj));
+        }
+        return booksToData;
+    }
+
+    @GetMapping("/products/{idK}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<ProductListToData> getProductsByCategory(@PathVariable int idK){
+        System.out.println("Get products by category");
+        List<Object[]> books = bookRepository.findBooksByCategory(Long.valueOf(idK));
+        List<ProductListToData> booksToData = new ArrayList<>();
+        for (Object[] bookObj:books){
+            booksToData.add(new ProductListToData(bookObj));
+        }
+        return booksToData;
+    }
 
 }
--- bookstore_backend/bookstore/src/main/java/com/listek/bookstore/loaders/BooksLoader.java
+++ bookstore_backend/bookstore/src/main/java/com/listek/bookstore/loaders/BooksLoader.java
@@ -1,14 +1,19 @@
 package com.listek.bookstore.loaders;
 
 import com.listek.bookstore.models.Book;
+import com.listek.bookstore.models.Category;
 import com.listek.bookstore.models.CoverType;
 import com.listek.bookstore.repositories.BookRepository;
+import com.listek.bookstore.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @Order(4)
@@ -16,6 +21,8 @@ public class BooksLoader implements CommandLineRunner {
 
     @Autowired
     BookRepository bookRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -24,15 +31,40 @@ public class BooksLoader implements CommandLineRunner {
 
 
     private void loadBooksData() {
+
         if (bookRepository.count() == 0) {
 
-            Book user1 = new Book(9.99, 10, "Kubuś Puchatek", "Książka dla dzieci",
+            List<Category> categories = categoryRepository.findAll();
+
+            Book book1 = new Book(9.99, 10, "Kubuś Puchatek",
+                    "Książka dla dzieci",
                     "Kubuś puchatek", "A.A. Milne", "NASZA KSIĘGARNIA",
                     LocalDateTime.of(2002, 9, 1, 0, 0),
                     367, "polski", "12345", CoverType.HardCover);
-            //Book user2 = new Book();
-            bookRepository.save(user1);
-            //bookRepository.save(user2);
+            book1.addCategory(categories.get(8));
+            Book book2 = new Book(19.99, 5, "Pan Tadeusz",
+                    "Polska epopeja narodowa",
+                    "Pan Tadeusz - Ostatni zjazd na Litwie", "Adam Mickiewicz",
+                    "NASZA KSIĘGARNIA",
+                    LocalDateTime.of(1997, 5, 23, 0, 0),
+                    578, "polski", "12346", CoverType.SoftCover);
+            book2.addCategory(categories.get(23));
+            Book book3 = new Book(9.99, 10, "Kordian",
+                    "Lektura Szkolna",
+                    "Kordian", "Juliusz Słowacki", "NASZA KSIĘGARNIA",
+                    LocalDateTime.of(2000, 8, 30, 0, 0),
+                    635, "polski", "12347", CoverType.HardCover);
+            book3.addCategory(categories.get(23));
+            Book book4 = new Book(29.99, 20, "Harry Potter i Komnata Tajemnic",
+                    "Książka o przygodach Harrego Pottera",
+                    "Harry Potter i Komnata Tajemnic", "J.K. Rowling", "NASZA KSIĘGARNIA",
+                    LocalDateTime.of(2007, 1, 1, 0, 0),
+                    426, "polski", "12320", CoverType.SoftCover);
+            book4.addCategory(categories.get(13));
+            bookRepository.save(book1);
+            bookRepository.save(book2);
+            bookRepository.save(book3);
+            bookRepository.save(book4);
         }
         System.out.println("Books: " + bookRepository.count());
     }
