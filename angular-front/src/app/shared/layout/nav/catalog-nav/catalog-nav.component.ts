f879e7e7930f5bbf1167eef7e51f1d3f2afa417f
maria.markowiak@gmail.com <maria.markowiak@gmail.com>
2023-01-16 13:47:32 +0100

    fetch categories from api

--- angular-front/src/app/shared/layout/nav/catalog-nav/catalog-nav.component.ts
+++ angular-front/src/app/shared/layout/nav/catalog-nav/catalog-nav.component.ts
@@ -1,14 +1,16 @@
 import { CategoriesService, Category } from 'src/app/core';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-catalog-nav',
   templateUrl: './catalog-nav.component.html',
   styleUrls: ['./catalog-nav.component.scss'],
 })
-export class CatalogNavComponent implements OnInit {
+export class CatalogNavComponent implements OnInit, OnDestroy {
   @Input() title: string = '';
   private categoriesService: CategoriesService;
+  private categoriesSub?: Subscription;
   categories: Category[] = [];
 
   constructor(categoriesService: CategoriesService) {
@@ -17,5 +19,13 @@ export class CatalogNavComponent implements OnInit {
 
   ngOnInit(): void {
     this.categories = this.categoriesService.getCategories();
+    this.categoriesService.categoriesChanged.subscribe((categories: Category[]) => {
+      this.categories = categories;
+      this.categories.forEach((cat) => console.log(cat.subcategories));
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.categoriesSub?.unsubscribe();
   }
 }
