bae5f294481e52fcdd53e199646e0c1ccc202785
Paulina Drzazga <paulina@halpress.team>
2023-01-18 12:48:28 +0100

    Fixed bug with removing item from cart

--- bookstore_backend/bookstore/src/main/java/com/listek/bookstore/services/CartService.java
+++ bookstore_backend/bookstore/src/main/java/com/listek/bookstore/services/CartService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -79,6 +80,7 @@ public class CartService {
     }
 
 
+    @Transactional
     public ResponseEntity removeCartItem(Long clientID, Long productID){
         Optional<Client> client = clientRepository.findClientById(clientID);
         return client
@@ -91,12 +93,11 @@ public class CartService {
                                         .map(foundCart -> {
                                             CartItem foundCartItem = foundCart.removeProductItem(foundProduct);
                                             if (foundCartItem != null) {
-                                                if (foundCartItem.getQuantity() == 0)
-                                                    cartItemRepository.save(foundCartItem);
-                                                else
-                                                    cartItemRepository.delete(foundCartItem);
+                                                cartItemRepository.save(foundCartItem);
                                                 productRepository.save(foundProduct);
                                                 cartRepository.save(foundCart);
+                                                cartItemRepository.removeEmptyCartItem();
+                                                cartRepository.removeEmptyCart();
                                                 System.out.println("Cart found. Product removed.");
                                                 return ResponseEntity.ok(HttpStatus.OK);
                                             } else {
